19a98cb2b44a0cb3bd5805f5db6c30e4573c3bb8
Francois Best <francois@francoisbest.com>
2021-11-20 00:11:10 +0100

    test: Add unit tests
    - Basics (port configuration)
    - Health check
    - Plugins (routes autoload & graceful exit)
    
    See #396.

---
+++ tests/health.test.ts
@@ -0,0 +1,81 @@
+import { createServer } from '../src'
+
+describe('Health checks', () => {
+  beforeEach(() => {
+    process.env.LOG_LEVEL = 'silent'
+  })
+
+  test('Default configuration exposes a /_health route', async () => {
+    const server = createServer()
+    await server.ready()
+    const res = await server.inject({ method: 'GET', url: '/_health' })
+    expect(res.statusCode).toEqual(200)
+    expect(res.json()).toEqual({ status: 'ok' })
+  })
+
+  test('Custom health check handler is called at startup and every 5 seconds', async () => {
+    jest.useFakeTimers()
+    const healthCheck = jest.fn().mockResolvedValue(true)
+    const server = createServer({
+      underPressure: {
+        healthCheck
+      }
+    })
+    await server.ready()
+    expect(healthCheck).toHaveBeenCalledTimes(1)
+    jest.advanceTimersByTime(5000)
+    expect(healthCheck).toHaveBeenCalledTimes(2)
+    jest.useRealTimers()
+  })
+
+  test('Custom health check throwing results in 503', async () => {
+    jest.useFakeTimers()
+    const healthCheck = jest
+      .fn()
+      .mockResolvedValueOnce(true) // First call passes (setup)
+      .mockResolvedValueOnce(true) // Second call passes (GET /_health 1)
+      .mockRejectedValueOnce(false) // Then it fails (GET /_health 2)
+    const server = createServer({
+      underPressure: {
+        healthCheck,
+        exposeStatusRoute: {
+          url: '/_health',
+          routeOpts: {
+            logLevel: 'silent'
+          }
+        }
+      }
+    })
+    await server.ready()
+    const res1 = await server.inject({ method: 'GET', url: '/_health' })
+    expect(res1.statusCode).toEqual(200)
+    expect(res1.json()).toEqual({ status: 'ok' })
+    jest.advanceTimersByTime(5000)
+    const res2 = await server.inject({ method: 'GET', url: '/_health' })
+    expect(res2.statusCode).toEqual(503)
+    expect(res2.json()).toEqual({
+      code: 'FST_UNDER_PRESSURE',
+      error: 'Service Unavailable',
+      message: 'Service Unavailable',
+      statusCode: 503
+    })
+    jest.useRealTimers()
+  })
+
+  test('Disabled health monitoring', async () => {
+    process.env.FASTIFY_MICRO_DISABLE_SERVICE_HEALTH_MONITORING = 'true'
+    jest.useFakeTimers()
+    const healthCheck = jest.fn().mockResolvedValue(true)
+    const server = createServer({
+      underPressure: {
+        healthCheck
+      }
+    })
+    await server.ready()
+    expect(healthCheck).not.toHaveBeenCalled()
+    jest.advanceTimersByTime(5000)
+    expect(healthCheck).not.toHaveBeenCalled()
+    jest.useRealTimers()
+    process.env.FASTIFY_MICRO_DISABLE_SERVICE_HEALTH_MONITORING = undefined
+  })
+})
---
+++ tests/plugins.test.ts
@@ -0,0 +1,55 @@
+import axios from 'axios'
+import { nanoid } from 'nanoid'
+import path from 'path'
+import { createServer, startServer } from '../src'
+import { delay } from './jigs'
+
+describe('Plugins', () => {
+  beforeEach(() => {
+    process.env.LOG_LEVEL = 'silent'
+  })
+
+  test('Loading routes from filesystem', async () => {
+    const routesDir = path.resolve(path.dirname(__filename), './routes')
+    const server = createServer({ routesDir })
+    await server.ready()
+    const res = await server.inject({ method: 'GET', url: '/foo' })
+    expect(res.statusCode).toEqual(200)
+    expect(res.json()).toEqual({ foo: 'bar' })
+  })
+
+  test('Graceful exit', async () => {
+    //jest.setTimeout(10000)
+    const key = nanoid()
+    const server = createServer()
+    server.get('/', async (_, res) => {
+      await delay(1000)
+      res.send({ key })
+    })
+    await startServer(server)
+    const resBeforeP = axios.get('http://localhost:3000/')
+    await delay(100) // Give time to the request to start before shutting down the server
+    await server.close()
+    const resBefore = await resBeforeP
+    expect(resBefore.data.key).toEqual(key)
+  })
+
+  test('Graceful exit with a slow onClose hook', async () => {
+    const key = nanoid()
+    const server = createServer()
+    server.get('/', async (_, res) => {
+      await delay(1000)
+      res.send({ key })
+    })
+    server.addHook('onClose', async (_, done) => {
+      await delay(3000) // Simulate slow shutdown of backing services
+      done()
+    })
+    await startServer(server)
+    const resBeforeP = axios.get('http://localhost:3000/')
+    await delay(100) // Give time to the request to start before shutting down the server
+    await server.close()
+    const resBefore = await resBeforeP
+    expect(resBefore.data.key).toEqual(key)
+  })
+})
